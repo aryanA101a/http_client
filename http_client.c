f5ccc7fd4d6bb5e88c9a14bd3cc7d77e227029de
Aryan Arora <aryanarora.w1@gmail.com>
2026-06-08 22:08:00 +0530

    0.00001

--- http_client.c
+++ http_client.c
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -62,41 +64,46 @@ void parse_url(char *url, url_t *result)
     }
 }
 
-void print_addrinfo(struct addrinfo *ai)
+void print_line(char *buf, size_t n)
 {
-    char host[NI_MAXHOST];
-    char serv[NI_MAXSERV];
-
-    int e = getnameinfo(ai->ai_addr, ai->ai_addrlen,
-                        host, sizeof(host),
-                        serv, sizeof(serv),
-                        NI_NUMERICHOST | NI_NUMERICSERV);
-
-    if (e != 0)
+    for (ssize_t i = 0; i < n; i++)
     {
-        printf("getnameinfo: %s\n", gai_strerror(e));
-        return;
-    }
+        unsigned char c = buf[i];
 
-    printf("family=%d socktype=%d protocol=%d addr=%s port=%s\n",
-           ai->ai_family,
-           ai->ai_socktype,
-           ai->ai_protocol,
-           host,
-           serv);
+        if (c == '\r')
+            printf("\\r");
+        else if (c == '\n')
+            printf("\\n\n");
+        else if (c == '\t')
+            printf("\\t");
+        else if (isprint(c))
+            putchar(c);
+        else
+            printf("\\x%02x", c);
+    }
 }
 
-int main()
+int http_get(char *url)
 {
-    url_t url;
+    url_t p_url;
     struct addrinfo *dns_res, *dns_res0;
     char req_buf[6114];
-    char res_buf[4096];
+    char res_buf[256];
+    char *line = NULL;
     int n = 0;
     int rn = 0;
+    size_t hn = 0;
+    int l_cap = 0;
     int e;
+    int c_len = -1;
+    int chunked = 0;
+    FILE *sfp = NULL;
+    FILE *dfile = NULL;
 
-    parse_url("http://httpbingo.org/get", &url);
+    parse_url(url, &p_url);
+
+    char *filename = !strcmp(p_url.path, "/") ? "unknown" : strrchr(p_url.path, '/') + 1;
+    dfile = fopen(filename, "wb");
 
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
@@ -105,17 +112,16 @@ int main()
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
 
-    e = getaddrinfo(url.hostname, url.protocol, &hints, &dns_res0);
+    e = getaddrinfo(p_url.hostname, p_url.protocol, &hints, &dns_res0);
     if (e != 0)
     {
         printf("%s\n", gai_strerror(e));
-        return 1;
+        goto cleanup;
     }
 
     int s = -1;
     for (dns_res = dns_res0; dns_res; dns_res = dns_res->ai_next)
     {
-        print_addrinfo(dns_res);
 
         s = socket(dns_res->ai_family,
                    dns_res->ai_socktype,
@@ -135,30 +141,159 @@ int main()
             continue;
         }
 
-        printf("connected!\n");
+        printf("connected!\n\n");
         break;
     }
 
     if (s < 0)
     {
         err(1, "%s", "cannot connect");
+        goto cleanup;
     }
     freeaddrinfo(dns_res0);
 
     n = snprintf(req_buf, sizeof(req_buf), "GET %s HTTP/1.1\r\n"
                                            "Host: %s\r\n"
-                                           "Connection: close\r\n"
                                            "User-Agent: aryan-http-client/0.1\r\n"
                                            "Accept: */*\r\n"
                                            "\r\n",
-                 url.path, url.hostname);
-    printf("%s", req_buf);
+                 p_url.path, p_url.hostname);
     write(s, req_buf, n);
 
-    while ((rn = read(s, res_buf, sizeof(res_buf))) > 0)
+    sfp = fdopen(s, "r+");
+    if (!sfp)
     {
-        fwrite(res_buf, 1, rn, stdout);
+        perror("fdopen");
+        goto cleanup;
     }
 
+    char version[16];
+    int status;
+
+    getline(&line, &l_cap, sfp);
+    printf("%s", line);
+    if (sscanf(line, "%15s %d", version, &status) != 2)
+    {
+        puts("bad status");
+        goto cleanup;
+    }
+
+    printf("version:%s status:%d\n\n", version, status);
+
+    char *col = NULL;
+    char *h_key = NULL;
+    char *h_val = NULL;
+    while ((hn = getline(&line, &l_cap, sfp)) > 0)
+    {
+
+        col = strchr(line, ':');
+
+        if (col)
+        {
+            *col = '\0';
+
+            h_key = line;
+            h_val = col + 1;
+
+            while (*h_val == ' ' || *h_val == '\t')
+                h_val++;
+
+            h_val[strcspn(h_val, "\r\n")] = '\0';
+
+            printf("key:%s value:%s\n", h_key, h_val);
+
+            if (strcasecmp(h_key, "Content-Length") == 0)
+            {
+                char *end;
+                long val;
+                val = strtol(h_val, &end, 10);
+                if (end == h_val)
+                {
+                    puts("invalid value");
+                }
+                c_len = val;
+            }
+            if (strcasecmp(h_key, "Transfer-Encoding") == 0 && strcasecmp(h_val, "Chunked") == 0)
+            {
+                chunked = 1;
+            }
+        }
+
+        if (strcmp(line, "\r\n") == 0)
+            break;
+    }
+
+    printf("\n");
+
+    if (c_len >= 0)
+    {
+        while (c_len)
+        {
+            size_t want = c_len < sizeof(res_buf) ? c_len : sizeof(res_buf);
+            rn = fread(res_buf, 1, want, sfp);
+            if (rn == 0)
+            {
+                puts("response truncated");
+                goto cleanup;
+            }
+            fwrite(res_buf, 1, rn, dfile);
+            c_len -= rn;
+        }
+    }
+    else if (chunked)
+    {
+        while (1)
+        {
+            size_t ch_len = -1;
+            if ((hn = getline(&line, &l_cap, sfp)) == 0)
+            {
+                puts("request truncated");
+                goto cleanup;
+            };
+
+            char *end;
+            long val;
+            val = strtol(line, &end, 16);
+            if (end == line)
+            {
+                puts("invalid value");
+                goto cleanup;
+            }
+            if (val == 0)
+            {
+                getline(&line, &l_cap, sfp);
+                break;
+            }
+
+            printf("bytes: %d\n", val);
+
+            if ((rn = fread(res_buf, 1, val, sfp)) > 0)
+            {
+                fwrite(res_buf, 1, rn, dfile);
+                fread(res_buf, 1, 2, sfp);
+            }
+        }
+    }
+    else
+    {
+        while ((rn = fread(res_buf, 1, sizeof(res_buf), sfp)) > 0)
+        {
+            fwrite(res_buf, 1, rn, dfile);
+        }
+    }
+
+    fflush(dfile);
+
+cleanup:
+    if (sfp)
+        fclose(sfp);
+    if (dfile)
+        fclose(dfile);
+
     return 0;
+}
+
+int main()
+{
+    http_get("http://httpbingo.org/image/jpeg");
 }
