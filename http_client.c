0abea8a74e9d97810158247d03d908012e33b8ac
Aryan Arora <aryanarora.w1@gmail.com>
2026-06-10 19:02:08 +0530

    0.00005

--- http_client.c
+++ http_client.c
@@ -8,7 +8,10 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <poll.h>
 #include <err.h>
+#include <errno.h>
+#include <time.h>
 
 typedef struct url
 {
@@ -33,7 +36,6 @@ void parse_url(char *url, url_t *result)
 {
     int pn = 0;
     int prn = 0;
-    int hn = 0;
     strcpy(result->protocol, "https");
     strcpy(result->hostname, "");
     strcpy(result->path, "/");
@@ -95,22 +97,132 @@ void print_line(char *buf, size_t n)
     }
 }
 
+int64_t now_ms(void)
+{
+    struct timespec ts;
+
+    if (clock_gettime(CLOCK_MONOTONIC, &ts) != 0)
+        return -1;
+
+    return (int64_t)ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+}
+
+ssize_t read_exact(int fd, void *buf, size_t n)
+{
+    int64_t start_ms = now_ms();
+
+    struct pollfd pfd = {.fd = fd, .events = POLLIN};
+    int ret;
+
+    while (1)
+    {
+        ret = poll(&pfd, 1, 250);
+        if (ret < 0)
+        {
+            if (errno == EINTR)
+                continue;
+            return -1;
+        }
+        if (ret == 0)
+        {
+            if (now_ms() - start_ms > 10000)
+            {
+                puts("idle timeout");
+                return -1;
+            }
+            continue;
+        }
+
+        if (pfd.revents & POLLIN)
+        {
+            ssize_t rn = read(fd, buf, n);
+            if (rn < 0 && errno == EINTR)
+                continue;
+
+            return rn;
+        }
+        if (pfd.revents & (POLLERR | POLLNVAL | POLLHUP))
+        {
+            errno = EIO;
+            return -1;
+        }
+    }
+}
+
+// should be initialized by {null,0,0} if not allocated at the initialization
+typedef struct
+{
+    char *buffer;
+    size_t capacity;
+    size_t size;
+} buf;
+
+int read_line(buf *buf, int fd)
+{
+    char c;
+    size_t len;
+    char *tmp;
+    size_t tmpsize;
+
+    if (buf->buffer == NULL)
+    {
+        if ((buf->buffer = malloc(512)) == NULL)
+        {
+            errno = ENOMEM;
+            return (-1);
+        }
+        buf->capacity = 512;
+    }
+
+    buf->buffer[0] = '\0';
+    buf->size = 0;
+
+    do
+    {
+        len = read_exact(fd, &c, 1);
+        if (len == -1)
+            return (-1);
+        if (len == 0)
+            break;
+
+        buf->buffer[buf->size++] = c;
+
+        if (buf->size == buf->capacity)
+        {
+            tmp = buf->buffer;
+            tmpsize = buf->capacity * 2 + 1;
+            if ((tmp = realloc(tmp, tmpsize)) == NULL)
+            {
+                errno = ENOMEM;
+                return (-1);
+            }
+            buf->buffer = tmp;
+            buf->capacity = tmpsize;
+        }
+
+    } while (c != '\n');
+
+    buf->buffer[buf->size] = '\0';
+
+    return (0);
+}
+
 int http_get(http_req_t req)
 {
     url_t p_url;
     struct addrinfo *dns_res, *dns_res0;
     char req_buf[6114];
-    char res_buf[256];
-    char *line = NULL;
+    char res_buf[2048];
     int n = 0;
-    size_t rn = 0;
-    size_t hn = 0;
-    int l_cap = 0;
+    ssize_t rn = 0;
     int e;
-    size_t c_len = -1;
+    ssize_t c_len = -1;
     int chunked = 0;
     FILE *sfp = NULL;
     FILE *dfile = NULL;
+    char version[16];
+    int status;
+    buf line = {.buffer = NULL, .capacity = 0, .size = 0};
 
     parse_url(req.url, &p_url);
 
@@ -172,19 +284,20 @@ int http_get(http_req_t req)
                  p_url.path, p_url.hostname);
     write(s, req_buf, n);
 
-    sfp = fdopen(s, "r+");
-    if (!sfp)
+    // sfp = fdopen(s, "r+");
+    // if (!sfp)
+    // {
+    //     perror("fdopen");
+    //     goto cleanup;
+    // }
+
+    if (read_line(&line, s) == -1)
     {
-        perror("fdopen");
         goto cleanup;
     }
 
-    char version[16];
-    int status;
-
-    getline(&line, &l_cap, sfp);
-    printf("%s", line);
-    if (sscanf(line, "%15s %d", version, &status) != 2)
+    // printf("%s", line.buffer);
+    if (sscanf(line.buffer, "%15s %d", version, &status) != 2)
     {
         puts("bad status");
         goto cleanup;
@@ -195,16 +308,20 @@ int http_get(http_req_t req)
     char *col = NULL;
     char *h_key = NULL;
     char *h_val = NULL;
-    while ((hn = getline(&line, &l_cap, sfp)) > 0)
+    do
     {
+        if (read_line(&line, s) == -1)
+        {
+            goto cleanup;
+        }
 
-        col = strchr(line, ':');
+        col = strchr(line.buffer, ':');
 
         if (col)
         {
             *col = '\0';
 
-            h_key = line;
+            h_key = line.buffer;
             h_val = col + 1;
 
             while (*h_val == ' ' || *h_val == '\t')
@@ -231,24 +348,28 @@ int http_get(http_req_t req)
             }
         }
 
-        if (strcmp(line, "\r\n") == 0)
-            break;
-    }
+    } while (strcmp(line.buffer, "\r\n") != 0);
 
     printf("\n");
-
     if (c_len >= 0)
     {
+        puts("content_length");
         size_t total = c_len;
         while (total)
         {
             size_t want = total < sizeof(res_buf) ? total : sizeof(res_buf);
-            rn = fread(res_buf, 1, want, sfp);
+            rn = read_exact(s, res_buf, want);
             if (rn == 0)
             {
                 puts("response truncated");
                 goto cleanup;
             }
+            if (rn < 0)
+            {
+                perror("read");
+                goto cleanup;
+            }
+
             fwrite(res_buf, 1, rn, dfile);
             total -= rn;
             req.on_progress(c_len - total, c_len);
@@ -256,45 +377,74 @@ int http_get(http_req_t req)
     }
     else if (chunked)
     {
+        puts("chunked");
+
         size_t down_n = 0;
         while (1)
         {
             size_t ch_len = -1;
-            if ((hn = getline(&line, &l_cap, sfp)) == 0)
+            if (read_line(&line, s) == -1)
             {
                 puts("request truncated");
                 goto cleanup;
             };
 
             char *end;
-            long val;
-            val = strtol(line, &end, 16);
-            if (end == line)
+            errno = 0;
+            unsigned long long val = strtoull(line.buffer, &end, 16);
+
+            if (end == line.buffer || errno == ERANGE)
             {
-                puts("invalid value");
+                puts("invalid chunk size");
                 goto cleanup;
             }
+
             if (val == 0)
             {
-                getline(&line, &l_cap, sfp);
+                read_line(&line, s);
                 break;
             }
 
             // printf("bytes: %d\n", val);
 
-            if ((rn = fread(res_buf, 1, val, sfp)) > 0)
+            size_t total = val;
+            while (total)
             {
+                size_t want = total < sizeof(res_buf) ? total : sizeof(res_buf);
+                rn = read_exact(s, res_buf, want);
+                if (rn == 0)
+                {
+                    puts("response truncated");
+                    goto cleanup;
+                }
+                if (rn < 0)
+                {
+                    perror("read");
+                    goto cleanup;
+                }
                 fwrite(res_buf, 1, rn, dfile);
-                fread(res_buf, 1, 2, sfp);
                 down_n += rn;
+                total -= rn;
                 req.on_progress(down_n, 0);
+            }
+
+            rn = read_exact(s, res_buf, 2);
+            if (rn == 0)
+            {
+                puts("response truncated");
+                goto cleanup;
+            }
+            if (rn < 0)
+            {
+                perror("read");
+                goto cleanup;
             }
         }
     }
     else
     {
         size_t down_n = 0;
-        while ((rn = fread(res_buf, 1, sizeof(res_buf), sfp)) > 0)
+        while ((rn = read_exact(s, res_buf, sizeof(res_buf))) > 0)
         {
             fwrite(res_buf, 1, rn, dfile);
             down_n += rn;
@@ -305,8 +455,8 @@ int http_get(http_req_t req)
     fflush(dfile);
 
 cleanup:
-    if (sfp)
-        fclose(sfp);
+    if (line.buffer)
+        free(line.buffer);
     if (dfile)
         fclose(dfile);
 
@@ -324,6 +474,8 @@ void on_progress(size_t received, size_t total)
 
 int main()
 {
+    // http://httpbingo.org/image/jpeg
+    // http://download.freebsd.org/snapshots/arm64/14.4-STABLE/kernel.txz
     http_req_t req = {.url = "http://download.freebsd.org/snapshots/arm64/14.4-STABLE/kernel.txz",
                       .on_progress = on_progress,
                       .sink = NULL};
